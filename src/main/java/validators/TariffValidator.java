44705cedbd0889d8f10d1d6b8730594990ec86d2
Natalia Makarchuk <32inter@gmail.com>
2018-10-23 17:45:57 +0300

    update TariffValidator.class

--- src/main/java/validators/TariffValidator.java
+++ src/main/java/validators/TariffValidator.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.List;
+import java.util.Set;
+
 @Component
 public class TariffValidator implements Validator {
 
@@ -17,11 +20,18 @@ public class TariffValidator implements Validator {
     @Override
     public void validate(Object o, Errors errors) {
         Tariff tariff=(Tariff)o;
+        Set<TariffOption> tariffOptions=tariff.getOptions();
 
         for (TariffOption op:tariff.getIncompatibleOptions()
-             ) {
+                ) {
             if (tariff.getOptions().contains(op))
-                errors.rejectValue("options","tariff.options.intersection1");
+                errors.rejectValue("incompatibleOptions","tariff.incompatibleOptions.intersection");
+        }
+
+        for (TariffOption option:tariffOptions) {
+            List<TariffOption> bad=option.getIncompatibleTariffOptions();
+            if (bad.stream().anyMatch(x->tariffOptions.contains(x)))
+                errors.rejectValue("options","tariff.options.intersection");
         }
 
     }
