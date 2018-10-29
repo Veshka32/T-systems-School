f62558677acd105c00265b063119c6aa7f009398
Natalia Makarchuk <32inter@gmail.com>
2018-10-29 19:17:28 +0300

    add Principal attribute to UserCabinet

--- src/main/java/controllers/UserCabinet.java
+++ src/main/java/controllers/UserCabinet.java
@@ -6,15 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import services.ClientService;
+import services.ContractService;
+
+import java.security.Principal;
 
 @Controller
 public class UserCabinet {
     @Autowired
-    ClientService clientService;
+    ContractService contractService;
 
     @RequestMapping("/user/cabinet")
-    public String create(@RequestParam("id") int id, Model model){
-        model.addAttribute("user",clientService.get(id));
+    public String create(Model model, Principal user){
+        model.addAttribute("user",contractService.findClientByPhone(Long.parseLong(user.getName())));
         return "user/user-cabinet";
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
     }
 }
