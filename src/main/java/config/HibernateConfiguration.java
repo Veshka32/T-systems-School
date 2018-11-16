83f526b1e2b9ab6c50200cd97ba14f98949749bb
Natalia Makarchuk <32inter@gmail.com>
2018-11-16 18:34:15 +0300

    migrate to wildfly

--- src/main/java/config/HibernateConfiguration.java
+++ src/main/java/config/HibernateConfiguration.java
@@ -1,11 +1,12 @@
 package config;
 
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+//import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -33,12 +34,15 @@ public class HibernateConfiguration {
 
     @Bean
     public DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(env.getProperty("db.driver"));
-        dataSource.setUrl(env.getProperty("db.url"));
-        dataSource.setUsername(env.getProperty("db.username"));
-        dataSource.setPassword(env.getProperty("db.password"));
-        return dataSource;
+        JndiDataSourceLookup jndiDataSourceLookup = new JndiDataSourceLookup();
+        return jndiDataSourceLookup.getDataSource("java:/mysql");
+
+        //        DataSource dataSource = new BasicDataSource();
+//        dataSource.setDriverClassName(env.getProperty("db.driver"));
+//        dataSource.setUrl(env.getProperty("db.url"));
+//        dataSource.setUsername(env.getProperty("db.username"));
+//        dataSource.setPassword(env.getProperty("db.password"));
+//        return dataSource;
     }
 
     @Bean
