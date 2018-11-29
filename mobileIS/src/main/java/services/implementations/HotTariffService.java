824643784aaa768032ed8e9fe9dd5df749583228
Natalia Makarchuk <32inter@gmail.com>
2018-11-29 13:04:23 +0300

    add HotTariffService; use it in HotTariffController instead of jmsSender

---
+++ mobileIS/src/main/java/services/implementations/HotTariffService.java
@@ -0,0 +1,49 @@
+package services.implementations;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import model.entity.Tariff;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import services.interfaces.HotTariffServiceI;
+import services.interfaces.JmsSenderI;
+import services.interfaces.TariffServiceI;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class HotTariffService implements HotTariffServiceI {
+
+    @Autowired
+    JmsSenderI jmsSender;
+
+    @Autowired
+    TariffServiceI tariffService;
+
+
+    @Override
+    @PostConstruct
+    public void postConstruct() {
+        pushHots();
+    }
+
+    @Override
+    public void pushHots() {
+        jmsSender.sendData(buildJson());
+    }
+
+    private String buildJson() {
+        List<Tariff> tariffList = tariffService.getLast(NUMBER_OF_HOT);
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        return gson.toJson(tariffList);
+    }
+
+    @Override
+    public void pushIfHots(int tariffId) {
+        List<Tariff> tariffList = tariffService.getLast(NUMBER_OF_HOT);
+        Optional<Tariff> any = tariffList.stream().filter(t -> t.getId() == tariffId).findFirst();
+        if (any.isPresent()) pushHots();
+    }
+}
