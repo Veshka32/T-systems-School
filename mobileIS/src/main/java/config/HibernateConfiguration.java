e7a2a1d9d60870d8465d8ca71675d0609608a28a
Natalia Makarchuk <32inter@gmail.com>
2018-12-03 17:57:02 +0300

    replace jndi (by wildfly) datasource with local datasource (dbcp +mysql connector)

--- mobileIS/src/main/java/config/HibernateConfiguration.java
+++ mobileIS/src/main/java/config/HibernateConfiguration.java
@@ -5,10 +5,8 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -33,18 +31,14 @@ public class HibernateConfiguration {
     }
 
     @Bean
-    @Profile("!test")
     public DataSource dataSource() {
-        JndiDataSourceLookup jndiDataSourceLookup = new JndiDataSourceLookup();
-        return jndiDataSourceLookup.getDataSource("java:/mysql");
-    }
-
-    @Bean
-    @Profile("test")
-    public DataSource dataSourceTest() {
+//        JndiDataSourceLookup jndiDataSourceLookup = new JndiDataSourceLookup();
+//        return jndiDataSourceLookup.getDataSource("java:/mysql");
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-        dataSource.setUrl(env.getProperty("jdbc.url"));
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
         return dataSource;
     }
 
